575219e0dd2e4e75f825a8661d2c4c4cc244e7ae
agegold <66872114+agegold@users.noreply.github.com>
2024-07-18 14:25:31 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -190,7 +190,7 @@ class CarState(CarStateBase):
     if not self.CP.carFingerprint in (CAR.NEXO):
       ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(gear))
       
-    
+    ret.gearShifter = GearShifter.drive
     if not self.CP.openpilotLongitudinalControl:
       aeb_src = "FCA11" if self.CP.flags & HyundaiFlags.USE_FCA.value else "SCC12"
       aeb_sig = "FCA_CmdAct" if self.CP.flags & HyundaiFlags.USE_FCA.value else "AEB_CmdAct"
@@ -263,7 +263,6 @@ class CarState(CarStateBase):
     #scc12 = cp.vl["SCC12"]
     #print("scc12_2=", scc12_2)
     #print("scc12=", scc12)
-    ret.gearShifter = GearShifter.drive
 
     return ret
 
